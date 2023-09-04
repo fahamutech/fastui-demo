4af2c9d18a10ca848a895d559aa1ef97a8db829c
joshuamshana <mama27j@gmail.com>
2023-09-04 11:55:09 +0300

    Implement intersection observation

--- src/blueprints/modules/core/components/log_element/logics/server_result_element.mjs
+++ src/blueprints/modules/core/components/log_element/logics/server_result_element.mjs
@@ -1,9 +1,41 @@
-
 /**
-* @param data {
-* {component: {states: *,inputs: *}, args: Array<*>}
-* }
-*/
+ * @param data {
+ * {component: {states: *,inputs: *}, args: Array<*>}
+ * }
+ */
 export function onStart(data) {
     // TODO: Implement the logic
+    let options = {
+        root: document.querySelector("#logs_container"),
+        rootMargin: "0px",
+        threshold: 0.01,
+    };
+
+    /**
+     *
+     * @param entries{IntersectionObserverEntry[]}
+     * @param observer1
+     */
+    const callback = (entries, observer1) => {
+        // console.log(entries, 'ENTRIES');
+        // entries.forEach(el=>{
+        // });
+        // const el = entries[0];
+        // console.log(el.target,'TARGET');
+        // console.log(el.intersectionRatio,'RATIO');
+        // console.log(el.intersectionRect,'RECT');
+        // console.log(el.isIntersecting,'INTERSECTING');
+        // console.log(el?.isVisible,'VISIBLE');
+        // console.log(observer1.takeRecords(), 'ENTRIES');
+        // console.log('-----------------');
+        // data.component.states.setVHeight(el.isIntersecting?'100%':10);
+    };
+    const observer = new IntersectionObserver(callback, options);
+    const {dataIndex} = data.component.states;
+    const target = document.querySelector(`#_${dataIndex}log_item`);
+    observer.observe(target);
+    return () => {
+        observer.unobserve(target);
+        observer.disconnect();
+    }
 }
