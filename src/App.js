3159f037e9cbd799042c55301b2f524bb1b4449e
joshuamshana <mama27j@gmail.com>
2023-09-05 20:03:13 +0300

    Add more demo cash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import {LoginContainer} from "./modules/core/components/login_form/login_container.mjs";
 import {ServerResults} from "./modules/core/components/login_form/server_results.mjs";
+import {BottomBox} from "./modules/core/components/register_form/bottom_box.mjs";
+import {BottomBoxB} from "./modules/core/components/register_form/bottom_box_b.mjs";
+import {SidebarBox} from "./modules/core/components/register_form/sidebar_box.mjs";
 
 function App() {
     return (
         <>
             {/*<ProfileCard/>*/}
-            <ServerResults/>
+            {/*<ServerResults/>*/}
+            <SidebarBox/>
             {/*<WelcomeText/>*/}
         </>
     );
