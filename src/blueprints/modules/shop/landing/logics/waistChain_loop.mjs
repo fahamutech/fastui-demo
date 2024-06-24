ab83c4e57fa9ff5bf7f9b1b61be164c7ec2e9353
joshuamshana <joshua.mshana@nmbtz.com>
2024-06-24 09:48:49 +0300

    Add more examples

--- src/blueprints/modules/shop/landing/logics/waistChain_loop.mjs
+++ src/blueprints/modules/shop/landing/logics/waistChain_loop.mjs
@@ -11,6 +11,6 @@ export function onStart(data) {
         {_key: '001', waist_index: '001', waist_name: 'Lee Style'},
         {_key: '002', waist_index: '002', waist_name: 'Usiku Kuchele'},
         {_key: '003', waist_index: '003', waist: 'https://picsum.photos/800/600'},
-        {_key: '004', waist_index: '004', waist: 'https://picsum.photos/800/600'},
+        {_key: '004', waist_index: '004', waist: 'https://picsum.photos/800/230'},
     ])
 }
